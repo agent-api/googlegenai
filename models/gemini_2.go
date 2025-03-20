43ce234b2d541a85a4c1a948be6896cfee4a579d
John McBirde <jpmmcbride@gmail.com>
2025-03-19 18:41:02 -0600

    feat: Models import "core" and not old "types"
    Signed-off-by: John McBirde <jpmmcbride@gmail.com>

--- models/gemini_2.go
+++ models/gemini_2.go
@@ -1,19 +1,19 @@
 package models
 
-import "github.com/agent-api/core/types"
+import "github.com/agent-api/core"
 
-var GEMINI_2_FLASH *types.Model = &types.Model{
+var GEMINI_2_FLASH *core.Model = &core.Model{
 	ID: "gemini-2.0-flash",
 }
 
-var GEMINI_2_FLASH_LITE *types.Model = &types.Model{
+var GEMINI_2_FLASH_LITE *core.Model = &core.Model{
 	ID: "gemini-2.0-flash-lite-preview",
 }
 
-var GEMINI_2_FLASH_THINKING *types.Model = &types.Model{
+var GEMINI_2_FLASH_THINKING *core.Model = &core.Model{
 	ID: "gemini-2.0-flash-thinking-exp",
 }
 
-var GEMINI_2_PRO *types.Model = &types.Model{
+var GEMINI_2_PRO *core.Model = &core.Model{
 	ID: "gemini-2.0-pro-exp-02-05",
 }
